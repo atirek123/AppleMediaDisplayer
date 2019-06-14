741d6d5e4d2ee9228a7afd0b68f90bb50bf35cd4
Atirek Sharma <atirek123@>
2019-06-14 14:19:02 +0530

    Initial Commit

---
+++ AppleMediaDisplayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleMediaDisplayer
+//
+//  Created by Atirek Sharma on 14/06/19.
+//  Copyright © 2019 Atirek Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
